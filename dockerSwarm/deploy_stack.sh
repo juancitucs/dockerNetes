7fe6b5c8d8c73831bb37c5f3bdbfd8c0d3e9693a
juancitucs <139298084+juancitucs@users.noreply.github.com>
2025-07-29 13:51:45 -0500

    Update deploy_stack.sh

--- dockerSwarm/deploy_stack.sh
+++ dockerSwarm/deploy_stack.sh
@@ -3,6 +3,6 @@
 STACK_NAME=wp_stack
 
 echo "Desplegando stack: $STACK_NAME"
-docker stack deploy -c wordpress-stack.yml $STACK_NAME
+sudo docker stack deploy -c wordpress-stack.yml $STACK_NAME
 
 echo "Verifica con: docker stack services $STACK_NAME"
