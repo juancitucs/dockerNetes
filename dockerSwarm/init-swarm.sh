7283ed9627d71e6b8369fb08dab8ee04773de6e2
juancitucs <139298084+juancitucs@users.noreply.github.com>
2025-07-29 13:52:01 -0500

    Update init-swarm.sh

--- dockerSwarm/init-swarm.sh
+++ dockerSwarm/init-swarm.sh
@@ -2,9 +2,9 @@
 # Solo se ejecuta en el nodo MANAGER
 
 echo "Inicializando Swarm..."
-docker swarm init --advertise-addr $(hostname -I | awk '{print $1}')
+sudo docker swarm init --advertise-addr $(hostname -I | awk '{print $1}')
 
 echo "Obteniendo token de worker..."
-docker swarm join-token worker
+sudo docker swarm join-token worker
 
 echo "Puedes unir tus nodos workers con ese token en ellos"
